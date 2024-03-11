7b2f9df00d6bae8e3ff1c30dc28e7e2985e491d3
José Sut <jose.sut@galileo.edu>
2024-03-10 23:09:10 -0600

    Elaborando el archivo php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $nombre = "José Sut";
+    echo "Hola mundo!, soy $nombre";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio php</title>
+</head>
+<body>
+    <img src="./gatitos_IA.jpg" alt="Imagen gatito IA">
+</body>
+</html>
